7150fc8e75ccdc8ea1ea1a2730727d9e1e3698f9
Steve Ayers <steveneayers@gmail.com>
2019-08-26 21:12:53 -0500

    pipeline

--- karma.conf.js
+++ karma.conf.js
@@ -17,13 +17,13 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
+      dir: require('path').join(__dirname, 'coverage'),
       reports: ['html', 'lcovonly', 'text-summary', 'cobertura'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml', 'junit'],
     junitReporter: {
-      outputDir: '../junit'
+      outputDir: 'junit'
     },
     port: 9876,
     colors: true,
