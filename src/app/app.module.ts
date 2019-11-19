58b371ec7221ab93d8785568e8813268304ddf6c
bluegeek9 <steveneayers@gmail.com>
2019-11-19 16:37:01 -0600

    Update jQuery to 3.4.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { EscapeHtmlPipe } from './pipes/keep-html.pipe';
@@ -45,6 +46,7 @@ import { MalihuScrollbarModule } from 'ngx-malihu-scrollbar';
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
+    NgbModule,
     MalihuScrollbarModule.forRoot()
   ],
   providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
