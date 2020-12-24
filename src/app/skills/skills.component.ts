2ecac9893fe7a60de69620949380c68f995be2b5
bluegeek9 <steveneayers@gmail.com>
2020-12-24 10:50:45 -0600

    Updates resume layout

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -10,6 +10,38 @@ export class SkillsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+	/* ---------------------------------------------------------------------- */
+	/* -------------------------------- skillbar ---------------------------- */
+	/* ---------------------------------------------------------------------- */
+
+	// $('.tabs-resume').click(function () {
+
+		$('.skillbar').each(function () {
+			$(this).find('.skillbar-bar').width(0);
+		});
+
+		$('.skillbar').each(function () {
+			$(this).find('.skillbar-bar').animate({
+				width: $(this).attr('data-percent')
+			}, 2000);
+		});
+
+	// });
+
+	// $('#resume').prev('h2.resp-accordion').click(function () {
+
+	// 	$('.skillbar').each(function () {
+	// 		$(this).find('.skillbar-bar').width(0);
+	// 	});
+
+	// 	$('.skillbar').each(function () {
+	// 		$(this).find('.skillbar-bar').animate({
+	// 			width: $(this).attr('data-percent')
+	// 		}, 2000);
+	// 	});
+	// });
+
   }
 
 }
