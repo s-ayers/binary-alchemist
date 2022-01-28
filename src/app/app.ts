dcb94091c03c6763a874350d53f735d946df74b0
bluegeek9 <steveneayers@gmail.com>
2022-01-28 10:38:34 -0600

    :lipstick: add Experience type

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,50 +13,50 @@ export const person = {
     {
       title: 'Software Engineer',
       company: 'Jack Henry & Associates',
-      start: '2015',
+      start: 2015,
       description: '<p>Create internal tools for internal users to automate common tasks: C#, MSSql, .NET Core, Angular, NodeJS, Azure DevOps CICD</p>'
     },
     {
       title: 'Contractor',
       company: 'Addision Group LLC',
-      start: '2013',
-      stop: '2015',
+      start: 2013,
+      stop: 2015,
       description: '<p>Contracted to Discovery Education for software develop. Build interactive textbooks. ReST API backend built with ColdFusion and MSSql. The frontend was build in Ember.</p>'
     },
     {
       title: 'Web Programmer',
       company: 'Barchart',
-      start: '2011',
-      stop: '2013',
+      start: 2011,
+      stop: 2013,
       description: '<p>A financial technology company providing data from dozens of exchanges. Barchart.com sells raw financial data, trading/analysis tools, widgets for 3rd party websites, and home to an active social network of traders on barchart.com.</p>'
     },
     {
       title: 'Software Engineer',
       company: 'Praxia Tech LLC',
-      start: '2010',
-      stop: '2011',
+      start: 2010,
+      stop: 2011,
       description: '<p>Provide technology services to small business and non-profits. Managed contacts with CiviCRM. Online stores with Magento.</p>'
     },
     {
       title: 'Network Engineer',
       company: 'Hostway',
-      start: '2008',
-      stop: '2010',
+      start: 2008,
+      stop: 2010,
       description: '<p>World\'s 5th largest web hosting company. Offering managed, un-managed, shared, and cloud hosting solutions for Linux and Windows.</p>'
     },
     {
       title: 'Supervisor',
       company: 'Support Techs LLC',
-      start: '2007',
-      stop: '2008',
+      start: 2007,
+      stop: 2008,
       description: '<p>Outsource call center serving customers with technical needs. Customers include CDW, Buffalo, Hawking, Belkin, Aerohive, Motorola, BFG, and many more.</p>'
     },
 
     {
       title: 'Technician',
       company: 'Geek-Ware',
-      start: '2003',
-      stop: '2006',
+      start: 2003,
+      stop: 2006,
       description: '<p>Provide on-site PC repair and network support for home users and small businesses.</p>'
     }
   ]
