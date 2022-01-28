c551bb1c388374b5a2733f1f682028dd73b8fa7f
bluegeek9 <steveneayers@gmail.com>
2022-01-27 22:19:55 -0600

    :fire: removes empty css files

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -6,7 +6,6 @@ import { MenuService } from '../services/menu.service';
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
 })
 export class BlogComponent extends PageComponent {
   routestring = '/blog';
